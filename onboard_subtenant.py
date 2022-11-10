9799329ea20028e06fd91f2a33020835c120a70d
parker <prwilliams5@gmail.com>
2022-11-10 14:27:13 -0600

    added subtenant variables and createTenant function

--- onboard_subtenant.py
+++ onboard_subtenant.py
@@ -11,7 +11,7 @@ morphheaders = {
 
 # subtenant variables.
 subten_name = morpheus['customOptions']['subtenName']
-subten_group = morpheus['customOptions']['subgroupname']
+# subten_group = morpheus['customOptions']['subgroupname']
 subten_domain = morpheus['customOptions']['subtenDomain']
 subten_currency = morpheus['customOptions']['subtenCurrency']
 
@@ -19,10 +19,7 @@ subten_currency = morpheus['customOptions']['subtenCurrency']
 subtenant_admin = morpheus['customOptions']['subtenAdminUsername']
 
 
-
-
 def createTenant():
-    
     url = f"https://{host}/api/accounts"
     payload = {"account": {
         "role": {"id": 2},
@@ -30,11 +27,9 @@ def createTenant():
         "subdomain": f"{subten_domain}",
         "currency": f"{subten_currency}"
     }}
-    
-    response = requests.post(url=url, json=payload, headers=morphheaders, verify=False)
-    
+
+    response = requests.post(url=url, json=payload, headers=morphheaders, verify=False) 
     data = response.json()
-    
     tenant_id = data['account']['id']
 
     print(tenant_id)
